f00232e2b2ca3b72d079554462c34653a1d50a77
Alessandro Bof <alessandro.bof@gmail.com>
2020-09-18 22:31:49 -0300

    Atualização dos autores.

--- snpbeta.py
+++ snpbeta.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-import argparse
 import sys
 import math
 
@@ -7,22 +6,22 @@ import math
 
 
 
+
+#definição da função beta
 def function_beta(p,n,z):
     beta=z/(math.sqrt(2*p*(1-p)*(n+z**2)))
     return beta
-
+#definição da função se
 def function_se(p,n,z):
     se=1/(math.sqrt(2*p*(1-p)*(n+z**2)))
     return se
 
-
-
-
-
+#aviso inicial
 print("SNPBeta v1.0")
+print("This software is distributed under: The GNU General Public License v3.0")
+print("Githup repository: https://github.com/alessandrobof/snpbeta")
 print("Authors:")
-print("D. L. Rovaris, B. S. Silva, A. B. Oliveira, C. S. Gusmão, J. K. N. Ramos")
-
+print("A. B. Oliveira, B. S. Silva, C. S. Gusmão, J. K. N. Ramos, D. L. Rovaris")
 
 
 #criando leitura de argumento com sys
@@ -35,7 +34,7 @@ foutd=sys.argv[2]
 
 fo=open(foutd,'w')
 print("Processing file: ",find)
-print("This might take several minutes...")
+print("This might take some minutes...")
 #lendo arquivo
 with open(find) as fp:
     #line=fp.readline()
@@ -63,9 +62,10 @@ with open(find) as fp:
             fo.write("\n")
         lc=lc+1
     
+#mensagem final    
 print("Processing finished.")
-print(" Snp processed: ",lc)
-
+print("Number of snp processed: ",lc)
+#fechando os arquivos
 fp.close()
 fo.close()
 
